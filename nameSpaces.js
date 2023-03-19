398f370e874b1843207ee99842fb530010bac7cc
quorthon1349 <119624955+quorthon1349@users.noreply.github.com>
2023-03-19 14:17:39 +0200

    Update nameSpaces.js

--- nameSpaces.js
+++ nameSpaces.js
@@ -1,6 +1,6 @@
 //серверные данные
 const SERVER = {
-  SERVER_URL: "http://api.openweathermap.org/data/2.5/weather",
+  SERVER_URL: "https://api.openweathermap.org/data/2.5/weather",
   API_KEY: "f472e0295e1d8eb30cf0021a2bb22aac",
 };
 
