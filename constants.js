f044d155f2d509aabe6763467755871b863d38ca
quorthon1349 <119624955+quorthon1349@users.noreply.github.com>
2023-03-19 13:56:22 +0200

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,29 @@
+const form = document.querySelector(".search-area");
+const favoriteCity = document.querySelector(".fav-city-name");
+const addFavoriteCityButton = document.querySelector(".fav");
+const enterCityField = document.querySelector(".search-field");
+
+const temp = document.querySelector(".tempValue");
+const imgLink = document.querySelector(".cloud");
+//for 2nd request
+const tempDetails = document.querySelector(".info-temp");
+const feelsLikeTemperature = document.querySelector(".feels-like-temp");
+const typeOfWeather = document.querySelector(".type-of-weather");
+const windSpeed = document.querySelector(".wind-speed");
+const sunriseTime = document.querySelector(".sunrise-time");
+const sunsetTime = document.querySelector(".sunset-time");
+
+export {
+  form,
+  favoriteCity,
+  addFavoriteCityButton,
+  enterCityField,
+  temp,
+  imgLink,
+  tempDetails,
+  feelsLikeTemperature,
+  typeOfWeather,
+  windSpeed,
+  sunriseTime,
+  sunsetTime,
+};
