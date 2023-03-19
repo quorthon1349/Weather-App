18d872139b9a74724042fa598d6ba34ce972abda
quorthon1349 <119624955+quorthon1349@users.noreply.github.com>
2023-03-19 13:57:10 +0200

    Create nameSpaces.js

---
+++ nameSpaces.js
@@ -0,0 +1,12 @@
+//серверные данные
+const SERVER = {
+  SERVER_URL: "http://api.openweathermap.org/data/2.5/weather",
+  API_KEY: "f472e0295e1d8eb30cf0021a2bb22aac",
+};
+
+//ошибки
+const ERROR = {
+  TRY_AGAIN: " Ошибка! Попробуйте повторить запрос позже",
+  ADD_INVALID_CITY: "Нельзя добавить пустой город!",
+};
+export { SERVER, ERROR };
